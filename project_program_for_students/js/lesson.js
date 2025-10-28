52ea5e97ca72a3cb4a63032e5048d75272bd82eb
Zhyldyz-prog <almasbekova_z@iuca.kg>
2025-10-29 00:56:09 +0600

    Laba_8

--- project_program_for_students/js/lesson.js
+++ project_program_for_students/js/lesson.js
@@ -16,7 +16,7 @@
 //         phoneSpan.style.color = '#800000';
 //     }
 // });
-
+// Российский номер
 const phoneInput = document.querySelector('#phone_input_for_russian');
 const phoneButton = document.querySelector('#phone_button_for_russian');
 const phoneSpan = document.querySelector('#phone_result_for_russian');
@@ -31,4 +31,74 @@ phoneButton.addEventListener('click', () => {
         phoneSpan.innerHTML = 'Этот номер не существует';
         phoneSpan.style.color = '#800000';
     }
-});
+})
+
+
+// TAB SLIDER
+ const tabsContentCards = document.querySelectorAll('.tab_content_block');
+ const tabsItems = document.querySelectorAll('.tab_content_item');
+ const tabsItemsParents = document.querySelector('.tab_content_items');
+
+ const highTabsContentCards = () =>{
+    tabsContentCards.forEach((tabsContentCard)=>{
+        tabsContentCard.style.display = 'none'
+    })
+    tabsItems.forEach((tabItem)=>{
+        tabItem.classList.remove('tab_content_item_active')
+    })
+ }
+
+ const showTabsContentCards = (indexElement = 0)=>{
+    tabsContentCards[indexElement].style.display = 'flex';
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+ }
+
+ highTabsContentCards();
+ showTabsContentCards();
+
+ // кликабельность 
+ tabsItemsParents.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex) =>{
+            if(event.target === tabItem){
+                highTabsContentCards()
+                showTabsContentCards(tabItemIndex)
+            }
+        })
+    }
+ }
+
+
+ let currentIndex = 0;// первая вскладка
+ let intervalId; // Переменная для хранения интервала
+
+ // Ф-ия для автоматического переключения
+
+ const startAuthoSlider = ()=>{
+    intervalId = setInterval(()=>{
+        highTabsContentCards();
+        showTabsContentCards(currentIndex);
+        currentIndex = (currentIndex +1) % tabsItems.length
+    }, 2000) //2 секунды (тк здесь не указывается в секундах указываем в милесекундах - 2000 =2 секунды))
+ }
+ //Запуск автослайдера
+ startAuthoSlider()
+
+ // Остановка слайдера при клике на вкладку
+ tabsItemsParents.onclick = (event) => {
+    clearInterval(intervalId);
+    if (event.target.classList.contains('tab_content_item')){
+        tabsItems.forEach((tabItem, tabItemIndex) => {
+            if(event.target === tabItem){
+                highTabsContentCards();
+                showTabsContentCards(tabItemIndex);
+                currentIndex = tabItemIndex
+                startAuthoSlider();
+            }
+        })
+    }
+ }
+
+
+
+
